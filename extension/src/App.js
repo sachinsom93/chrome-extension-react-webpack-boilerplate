eb9aebd2b98b5c4c8bf4d6348721fcab498632b5
Sachin Som <sachinsom507@gmail.com>
2023-01-26 12:18:41 +0530

    Dev: Webpack, eslint, prettier and manifest configuration.

---
+++ extension/src/App.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function App() {
+	return <React.Fragment>Chrome Extension Boiler plate</React.Fragment>;
+}
---
+++ extension/src/content-scripts/content.js
@@ -0,0 +1,5 @@
+(function () {
+	
+	const waitTimesTable = document.getElementById('table_wait_times_rwd');
+	console.log(waitTimesTable);
+})();
